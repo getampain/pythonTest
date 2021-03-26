67cd993e2b025f44ed8e2f6aa7f872eb464e9007
swyoo <getampain@naver.com>
2021-03-26 16:16:38 +0900

    add

--- multiprocessing/poolTest.py
+++ multiprocessing/poolTest.py
@@ -1,10 +1,16 @@
+#멀티프로세싱 pool 기능 실험해보기
+#20210326 swyoo
+##################################
+
 import time
 import multiprocessing
 
-def innerLoop(i_num):
+from functools import partial
+
+def innerLoop(userName, dictionOrd, i_num):
     for j in range(0, 10):
         time.sleep(delay_time)
-        print(str(i_num) + " : " + str(j) + " \n")
+        print(userName + str(i_num) + " : " + str(j) + " \n")
 
 
 star_time = time.time()
@@ -15,16 +21,14 @@ delay_time = 1.0
 
 
 
-#print("nowTime : %s" % (time.time() - star_time))
-#time.sleep(3)
-#print(123)
-
-#
-
+#main에서 시작되지 않으면 pool은 작동하지 않는다
 if __name__ == '__main__':
 
     pool = multiprocessing.Pool(processes=5)
-    pool.map(innerLoop, range(0,10) )
+
+    #partial을 통하여 함수에 미리 변수를 채워둔다
+    func = partial(innerLoop, "hello? : ", {"abc":123, "def":456})
+    pool.map(func, range(0,10))
 
     #normal case 
     """
